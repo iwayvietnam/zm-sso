30a298e9862d4e2ff1246f67a56e11b68b5b433c
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-10-12 09:14:15 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- config/settings.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-use Monolog\Logger;
-use Slim\App;
-
-return function (App $app) {
-    $container = $app->getContainer();
-    $container->add('settings', include 'config.php');
-};
