23c164bba680a9055bf9bcdded40d17cb0342efe
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2021-04-29 11:19:28 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- src/main/java/com/iwayvietnam/zmsso/pac4j/SettingsBuilder.java
+++ src/main/java/com/iwayvietnam/zmsso/pac4j/SettingsBuilder.java
@@ -137,9 +137,7 @@ public final class SettingsBuilder {
 
     private static void loadSettingsFromLocalConfig() {
         final var fields = Arrays.asList(SettingsConstants.class.getDeclaredFields());
-        Arrays.asList(PropertiesConstants.class.getDeclaredFields()).forEach(field -> {
-            fields.add(field);
-        });
+        Arrays.asList(PropertiesConstants.class.getDeclaredFields()).forEach(field -> fields.add(field));
         fields.forEach(field -> {
             try {
                 final var key = field.get(null).toString();
