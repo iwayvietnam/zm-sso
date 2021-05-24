a6b1983e55a2d47c686dbbd8aec3e36582a3ccb2
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2021-05-24 13:30:48 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- src/main/java/com/iwayvietnam/zmsso/saml/SamlSloHandler.java
+++ src/main/java/com/iwayvietnam/zmsso/saml/SamlSloHandler.java
@@ -3,7 +3,8 @@ package com.iwayvietnam.zmsso.saml;
 import com.zimbra.cs.extension.ExtensionException;
 import org.opensaml.saml.common.xml.SAMLConstants;
 import org.pac4j.core.context.JEEContext;
-import org.pac4j.core.exception.http.RedirectionActionHelper;
+import org.pac4j.core.context.session.JEESessionStore;
+import org.pac4j.core.util.HttpActionHelper;
 import org.pac4j.saml.logout.impl.SAML2LogoutResponseBuilder;
 import org.pac4j.saml.logout.impl.SAML2LogoutResponseMessageSender;
 
@@ -35,16 +36,18 @@ public class SamlSloHandler extends SamlBaseHandler {
         final var saml2LogoutResponseMessageSender = new SAML2LogoutResponseMessageSender(client.getSignatureSigningParametersProvider(),
                 spLogoutResponseBindingType, false, client.getConfiguration().isSpLogoutRequestSigned());
 
-        final var samlContext = contextProvider.buildContext(context);
+        final var samlContext = contextProvider.buildContext(client, context, JEESessionStore.INSTANCE);
+        samlContext.setSaml2Configuration(client.getConfiguration());
         logoutProfileHandler.receive(samlContext);
         final var logoutResponse = saml2LogoutResponseBuilder.build(samlContext);
-        saml2LogoutResponseMessageSender.sendMessage(samlContext, logoutResponse, samlContext.getSAMLBindingContext().getRelayState());
+        saml2LogoutResponseMessageSender.sendMessage(samlContext, logoutResponse,
+                samlContext.getSAMLBindingContext().getRelayState());
 
         final var adapter = samlContext.getProfileRequestContextOutboundMessageTransportResponse();
         if (spLogoutResponseBindingType.equalsIgnoreCase(SAMLConstants.SAML2_POST_BINDING_URI)) {
-            throw RedirectionActionHelper.buildFormPostContentAction(context, adapter.getOutgoingContent());
+            throw HttpActionHelper.buildFormPostContentAction(context, adapter.getOutgoingContent());
         } else {
-            throw RedirectionActionHelper.buildRedirectUrlAction(context, adapter.getRedirectUrl());
+            throw HttpActionHelper.buildRedirectUrlAction(context, adapter.getRedirectUrl());
         }
     }
 
